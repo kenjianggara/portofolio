313902522f093ac06a00557d18496dcb40ef806e
kenjianggara <kenjikeren@yahoo.co.id>
2025-10-17 09:42:16 +0700

    Delete data/config.dev.php
    sensitive information inside (even dev)

--- data/config.dev.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-  'posts_file'      => __DIR__ . './posts.json',
-  'posts_hmac_file' => __DIR__ . './posts.hmac',
-  'secret_key'      => '0123456789abcdef0123456789abcdef',
-  'admin_user'      => 'admin',
-  'admin_pass_hash' => password_hash('DevPass123!', PASSWORD_DEFAULT),
-];
